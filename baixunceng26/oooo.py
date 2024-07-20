a15896f21f65367ba44cc0b3306e70e31621171d
000 <150587267+baixunceng26@users.noreply.github.com>
2024-07-20 18:58:01 +0800

    Update oooo.py

--- baixunceng26/oooo.py
+++ baixunceng26/oooo.py
@@ -6,13 +6,11 @@ bot = telebot.TeleBot('7157007495:AAEfts9nkfVzWQSYIytVZ-CsBl7Jp1loDcQ')
 @bot.message_handler(commands=['add'])
 def add_food(message):
     bot.reply_to(message, "Okay")
+    del curriculum [int((message.text[5])-1)]
     curriculum.insert((int(message.text[5])) - 1, message.text[6:])
-    print((int(message.text[5]))-1,message.text[6:])
+
 @bot.message_handler(commands=['delete'])
 def add_food(message):
-    bot.reply_to(message, "which one do you want to delete")
-    @bot.message_handler(commands=['elete'])
-    def add_food(message):
         bot.reply_to(message, "Okay")
         del curriculum [int((message.text[7:])-1)]
         curriculum.insert [int((message.text[7:])-1),int('no class')]
